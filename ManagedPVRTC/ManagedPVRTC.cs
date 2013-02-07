ee9e52f744c1962b77a2a6d8ee6cb23d2a507ea4
Ray Batts <Ray@Sickheadgames.com>
2013-02-06 21:07:38 -0600

    Initial commit.

---
+++ ManagedPVRTC/ManagedPVRTC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ManagedPVRTC
+{
+    public static class ManagedPVRTC
+    {
+        [DllImport("PVRTexLibC", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr CompressTexture(byte[] data, int height, int width, int mipLevels, bool preMultiplied, bool pvrtc4bppCompression, ref IntPtr dataSizes);
+    }
+}
