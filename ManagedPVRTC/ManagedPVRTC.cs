ff08876c8548b2ce932265c219ebb8f4c6df4469
Ray Batts <Ray@Sickheadgames.com>
2013-03-01 15:43:25 -0600

    Update for new dll name.

--- ManagedPVRTC/ManagedPVRTC.cs
+++ ManagedPVRTC/ManagedPVRTC.cs
@@ -8,7 +8,7 @@ namespace ManagedPVRTC
 {
     public static class ManagedPVRTC
     {
-        [DllImport("PVRTexLibC", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("PVRTexLibWrapper", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr CompressTexture(byte[] data, int height, int width, int mipLevels, bool preMultiplied, bool pvrtc4bppCompression, ref IntPtr dataSizes);
     }
 }
